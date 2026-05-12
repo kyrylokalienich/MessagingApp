1c64c0b1df240604c921b8c57a6bef3dbbd2be52
kosars <155952445+kyrylokalienich@users.noreply.github.com>
2026-05-13 01:08:13 +0300

    chore(contracts): move IUserRepositoty

--- Messaging.Contracts/Repository/IMessageRepository.cs
+++ Messaging.Contracts/Repository/IMessageRepository.cs
@@ -8,12 +8,3 @@ public interface IMessageRepository
     IReadOnlyList<Message> GetAll();
     void Save();
 }
-
-public interface IUserRepository
-{
-    void Add(User user);
-    User? FindByLogin(string login);
-    User? FindByEmail(string email);
-    bool LoginExists(string login);
-    void Save();
-}
---
+++ Messaging.Contracts/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Messaging.Domain;
+
+namespace Messaging.Contracts.Repository;
+
+public interface IUserRepository
+{
+    void Add(User user);
+    User? FindByLogin(string login);
+    User? FindByEmail(string email);
+    bool LoginExists(string login);
+    void Save();
+}
